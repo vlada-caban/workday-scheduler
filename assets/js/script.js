b02a171bac8e2103e63be3b116d549f7a1f85fc9
vlada_caban <vladaonly@gmail.com>
2023-04-23 20:17:51 -0400

    added local storange working rendering

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,13 +10,13 @@ $(function () {
   // useful when saving the description in local storage?
   //
 
-  //today's date
-const today = dayjs();
-$("#currentDay").text(today.format("MMM D, YYYY"));
+  //getting today's date
+  const today = dayjs();
+  $("#currentDay").text(today.format("MMM D, YYYY"));
 
-  //gets current hour
+  //getting current hour
   let currentHour = dayjs().hour();
-  console.log(currentHour);
+  //console.log(currentHour);
 
   //let currentHour =17;
 
@@ -24,16 +24,22 @@ $("#currentDay").text(today.format("MMM D, YYYY"));
   const containerAllHours = $(".container-lg");
 
   const availableHours = [
-      "9 AM",
-      "10 AM",
-      "11 AM",
-      "12 PM",
-      "1 PM",
-      "2 PM",
-      "3 PM",
-      "4 PM",
-      "5 PM",
-    ]; 
+    "9 AM",
+    "10 AM",
+    "11 AM",
+    "12 PM",
+    "1 PM",
+    "2 PM",
+    "3 PM",
+    "4 PM",
+    "5 PM",
+  ];
+
+  let localStorageData = JSON.parse(localStorage.getItem("schedule"));
+  console.log(localStorageData);
+
+  let taskToRender = "";
+  let timeOfTaskToRender = "";
 
   for (let i = 0; i < availableHours.length; i++) {
     //creating div for each hour without past/present/future classes
@@ -42,83 +48,89 @@ $("#currentDay").text(today.format("MMM D, YYYY"));
 
     let hour = parseInt(availableHours[i].split(" ")[0]);
     let timeOfTheDay = availableHours[i].split(" ")[1];
-    let formattedHour = 0;  
-     if (timeOfTheDay === "PM" && hour !== 12) {
+    let formattedHour = 0;
+    if (timeOfTheDay === "PM" && hour !== 12) {
       formattedHour = hour + 12;
     } else {
       formattedHour = hour;
     }
+    //adding past/present/future classes based on the current hour
+    if (currentHour === formattedHour) {
+      hourDiv.addClass("present");
+    } else if (currentHour > formattedHour) {
+      hourDiv.addClass("past");
+    } else {
+      hourDiv.addClass("future");
+    }
 
-if (currentHour === formattedHour) {
-  hourDiv.addClass("present");
-} else if (currentHour > formattedHour) {
-  hourDiv.addClass("past");
-} else {
-  hourDiv.addClass("future");
-}
     const hourTitleDiv = $("<div>");
     hourTitleDiv.text(availableHours[i]);
-    hourTitleDiv.addClass("col-2 col-md-1 hour text-center py-3"); //can I do this?
+    hourTitleDiv.addClass("col-2 col-md-1 hour text-center py-3");
+    console.log(availableHours[i]);
 
     const taskDescription = $("<textarea>");
     taskDescription.addClass("col-8 col-md-10 description");
-    taskDescription.attr("rows", "3"); 
+    taskDescription.attr("rows", "3");
+
+    if (localStorageData !== null) {
+      for (let j = 0; j < localStorageData.length; j++) {
+        timeOfTaskToRender = localStorageData[j].taskHour;
+        taskToRender = localStorageData[j].taskContent;
+        // console.log("Time: " + timeOfTaskToRender);
+        // console.log("Task: " + taskToRender);
+        if (availableHours[i] === timeOfTaskToRender) {
+          taskDescription.text(taskToRender);
+        }
+      }
+    }
 
     const saveBtn = $("<button>");
     saveBtn.addClass("btn saveBtn col-2 col-md-1");
-    saveBtn.attr("aria-label", "save"); 
-
-    saveBtn.on("click", function(event){
-event.preventDefault();
-//console.log(event.target.type);
-
-let targetTextArea;
-if (event.target.type === undefined) {
-  targetTextArea = event.target.parentElement.previousElementSibling;
-} else if (event.target.type === "submit") {
-  targetTextArea = event.target.previousElementSibling;
-} 
-let hour = targetTextArea.previousElementSibling.innerHTML.split(" ")[0];
-
-console.log(hour);
-//console.log(targetTextArea);
-
-let scheduleDetails = {
-  taskContent: targetTextArea.value,
-  taskHour: hour
-};
-
-let localStorageData = JSON.parse(localStorage.getItem("schedule"));
-
-if (localStorageData === null) {
-localStorageData = []
-localStorageData.push(scheduleDetails)
-} else {
-  localStorageData.push(scheduleDetails);
-}
-
-
-  localStorage.setItem("schedule", JSON.stringify(localStorageData));
-
-    })
+    saveBtn.attr("aria-label", "save");
 
     const iArea = $("<i>");
     iArea.addClass("fas fa-save");
     iArea.attr("aria-hidden", "true");
 
-    // iArea.on("click", function (event) {
-    //   event.preventDefault();
-    //   console.log(event.target.previousElementSibling.previousElementSibling);
-    // });
-
     saveBtn.append(iArea);
     hourDiv.append(hourTitleDiv, taskDescription, saveBtn);
 
     containerAllHours.append(hourDiv);
-   
+
+    saveBtn.on("click", function (event) {
+      event.preventDefault();
+      //console.log(event.target.type);
+
+      let targetTextArea;
+
+      if (event.target.type === undefined) {
+        targetTextArea = event.target.parentElement.previousElementSibling;
+      } else if (event.target.type === "submit") {
+        targetTextArea = event.target.previousElementSibling;
+      }
+
+      // let hourSelected = targetTextArea.previousElementSibling.innerHTML.split(" ")[0];
+
+      let hourSelected = targetTextArea.previousElementSibling.innerHTML;
+
+      //console.log(hourSelected);
+      //console.log(targetTextArea);
+
+      let scheduleDetails = {
+        taskContent: targetTextArea.value,
+        taskHour: hourSelected,
+      };
+
+      if (localStorageData === null) {
+        localStorageData = [];
+        localStorageData.push(scheduleDetails);
+      } else {
+        localStorageData.push(scheduleDetails);
+      }
+
+      localStorage.setItem("schedule", JSON.stringify(localStorageData));
+    });
   }
-
-
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -133,5 +145,4 @@ localStorageData.push(scheduleDetails)
   //
 
   // TODO: Add code to display the current date in the header of the page.
-  
 });
